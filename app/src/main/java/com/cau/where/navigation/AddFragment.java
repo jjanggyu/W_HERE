1b7a7c9d56e5248af189743992be500466d0a2dd
BeJ Park <bjpark0506@naver.com>
2020-10-29 22:35:44 +0900

    갤러리부분수정하는중이었음

--- app/src/main/java/com/cau/where/navigation/AddFragment.java
+++ app/src/main/java/com/cau/where/navigation/AddFragment.java
@@ -1,21 +1,63 @@
 package com.cau.where.navigation;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.cau.where.R;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class AddFragment extends Fragment {
+    final int REQ_CODE_SELECT_IMAGE =100;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add,container,false);
+        ImageView imageView = (ImageView)view.findViewById(R.id.add_photo);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,REQ_CODE_SELECT_IMAGE);
+            }
+        });
         return view;
     }
+//    private  void setupViewPager(){
+//        PagerAdapter adapter = new PagerAdapter(getChildFragmentManager()) {
+//            @Override
+//            public int getCount() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+//                return false;
+//            }
+//        };
+//    }
+
+
 }
